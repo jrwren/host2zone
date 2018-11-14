b1133e66a64988acc4018930967e4406d8d4b04a
jawren <jawren@cisco.com>
2018-11-14 17:13:07 -0500

    make soa configurable

--- main.go
+++ main.go
@@ -12,8 +12,15 @@ import (
 )
 
 func main() {
-	var zone string
+	var ns, zone string
+	var ttl, refresh, retry, expire, negative_cache int
+	flag.StringVar(&ns, "ns", "ns.example.com", "the name of the ns server")
 	flag.StringVar(&zone, "zone", "example.com", "the name of the dns zone")
+	flag.IntVar(&ttl, "ttl", 120, "the ttl of the dns zone")
+	flag.IntVar(&refresh, "refresh", 604800, "the secondary refresh of the dns zone")
+	flag.IntVar(&retry, "retry", 86400, "the secondary retry of the dns zone")
+	flag.IntVar(&expire, "expire", 2419200, "the secondary expire of the dns zone")
+	flag.IntVar(&negative_cache, "negative_cache", 604800, "the negative_cache of the dns zone")
 	flag.Parse()
 	var err error
 	var r io.ReadCloser
@@ -26,7 +33,8 @@ func main() {
 		}
 	}
 	serial := time.Now().Format("0601021504") // YYMMDDHHmmSS
-	fmt.Printf(soaTemplate, zone, zone, serial, zone)
+	fmt.Printf(soaTemplate, ttl, zone, zone, serial, refresh, retry, expire,
+		negative_cache, ns)
 	s := bufio.NewScanner(r)
 	for s.Scan() {
 		line := s.Text()
@@ -36,21 +44,26 @@ func main() {
 		}
 		ip := fields[0]
 		for _, host := range fields[1:] {
-			if host == "" {
+			if host == "" || strings.Contains(ip, "%") {
 				continue
 			}
+			if strings.Contains(ip, ":") {
+				fmt.Printf("%s IN AAAA %s\n", host, ip)
+				continue
+			}
+			host = strings.Replace(host, "."+zone, "", -1)
 			fmt.Printf("%s IN A %s\n", host, ip)
 		}
 	}
 }
 
-var soaTemplate string = `$TTL    604800
+var soaTemplate string = `$TTL    %d
 @       IN      SOA     %s. hostmaster@%s. (
-                             %s          ; Serial
-                         604800         ; Refresh
-                          86400         ; Retry
-                        2419200         ; Expire
-                         604800 )       ; Negative Cache TTL
+                             %s         ; Serial
+                             %d         ; Refresh
+                             %d         ; Retry
+                             %d         ; Expire
+                             %d )       ; Negative Cache TTL
 ;
 @                       IN      NS              %s.
 `
